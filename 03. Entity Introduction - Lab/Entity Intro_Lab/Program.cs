24640c9b1aa0ce7b5f6f92d9d8460097f5f1f19c
Zornitsa Muletarova <79112032+ZMuletarova@users.noreply.github.com>
2021-10-02 23:39:33 +0300

    add

---
+++ 03. Entity Introduction - Lab/Entity Intro_Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Entity_Intro_Lab.Models;
+namespace Entity_Intro_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
